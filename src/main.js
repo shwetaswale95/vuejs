c5518a253fe6f55a25eddd0cbc876643dbe17caf
shwetaswale95 <sh.aswale95@gmail.com>
2023-08-21 15:19:27 +0530

    integrate tailwind css

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import router from "./router";
+// add this
+import "./index.css";
 
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue);
